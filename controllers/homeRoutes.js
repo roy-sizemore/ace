a340b8cbf67de9af5166f5a85b61f7d1a506babe
Roy Sizemore <rsizemore@swampcastle.home>
2021-04-29 20:29:29 -0400

    Added api folder and file in routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,18 +1,13 @@
 const router = require('express').Router();
-const homeRoutes = require('homeRoutes');
-const apiRoutes = require('api');
+const user = require('../seeds/userData.json');
+const apiRoutes = require('/api');
 
-router.get('[*****FILEPATH NEEDED*****]', (req, res) => {
-  return res.json(compData);
+router.get('/api', (req, res) => {
+  return res.json(apiRoutes);
 })
   .catch((err) => res.status(500).json(err));
 
-router.get('[*****FILEPATH NEEDED*****', (req, res) => {
-  return res.json(trainingData);
-})
-  .catch((err) => res.status(500).json(err));
-
-router.post('/api', (req, res) => {
+router.post('user', (req, res) => {
   return res.json(user);
 })
   .catch((err) => res.status(500).json(err));
