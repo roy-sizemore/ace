d819f950650cb81f9822a1778c76b20a4221ea4d
HRush <hrkoren@gmail.com>
2021-05-01 11:10:05 -0400

    removed pdfkit require from server.js; this may need to go somewhere else

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
-const PDFDocument = require('pdfkit');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
