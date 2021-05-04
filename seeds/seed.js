cbb879871ce37b6844cd16302454580117e92ad1
HRush <hrkoren@gmail.com>
2021-05-03 20:47:17 -0400

    working through errors to seed db

--- seeds/seed.js
+++ seeds/seed.js
@@ -13,12 +13,12 @@ const seedDatabase = async () => {
         individualHooks: true,
         returning: true,
     });
-    for (const comp of compData) {
-        const newComp = await Comp.create({
-            ...comp,
-            user_id: users[Math.floor(Math.random() * users.length)].id,
-        });
-    }
+    // for (const comp of compData) {
+    //     const newComp = await Comp.create({
+    //         ...comp,
+    //         user_id: users[Math.floor(Math.random() * users.length)].id,
+    //     });
+    // }
     process.exit(0);
 };
 
