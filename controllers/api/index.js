efdd5132c98c6744cadbf0248dd2c88860f3af97
Roy Sizemore <rsizemore@swampcastle.home>
2021-05-04 20:23:33 -0400

    Added legacy routes directory. Moved potentially unnecessary routes to legacy folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,12 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const trainingRoutes = require('./trainingRoutes');
 const competencyRoutes = require('./competencyRoutes');
-const skillRoutes = require('./skillRoutes');
+// May not need skillRoutes (see legacy folder)
+// const skillRoutes = require('./skillRoutes');
 
 router.use('/users', userRoutes);
 router.use('/training', trainingRoutes);
 router.use('/competencies', competencyRoutes);
-router.use('/skills', skillRoutes);
+// router.use('/skills', skillRoutes);
 
 module.exports = router;
--- controllers/api/legacy/skillRoutes.js
+++ controllers/api/legacy/skillRoutes.js
@@ -2,6 +2,4 @@ const router = require('express').Router();
 const { SkillLevel } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', (req,res)=> {})
-
 module.exports = router;
