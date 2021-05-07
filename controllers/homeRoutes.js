097f01cb6741beb6fc8dd45595b8a2afe1c0f757
HRush <hrkoren@gmail.com>
2021-05-07 18:24:09 -0400

    updated routes, login js, removed legacy html files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,25 +1,25 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
-const {User} = require('../models');
+const { User } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('login')
-  })
+  res.render('login')
+})
 
-  router.get('/login', (req, res) => {
-    res.render('login');
-  });
-  
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
 router.get('/training', withAuth, async (req, res) => {
   const profile = await User.findAll({
     attributes: ['name', 'email', 'id']
-  });
+  })
   res.render('trainer');
 })
 
-router.get('/competencies', withAuth, (req, res) => {
-  const trainee = User.findByPk(req.params.id);
+router.get('/competencies/:id', withAuth, (req, res) => {
+  User.findByPk(req.params.id);
   res.render('trainee');
 });
- 
+
 module.exports = router;
