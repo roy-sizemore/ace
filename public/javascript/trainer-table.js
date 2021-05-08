2749f190661fe1f177012dd8757e4ce68c9321a7
HRush <hrkoren@gmail.com>
2021-05-07 20:46:57 -0400

    added table js file

---
+++ public/javascript/trainer-table.js
@@ -0,0 +1,16 @@
+$(function () {
+    let templateText = $('#tableTemplate').html();
+
+    let tableTeamplate = Handlebars.compile(templateText);
+
+    let users = [
+        { "Employee Name": "Roy", "E-Mail": "roy@email.com", "ID #": 01 },
+        { "Employee Name": "Tre", "E-Mail": "tre@email.com", "ID #": 02 },
+        { "Employee Name": "Heather", "E-Mail": "heather@email.com", "ID #": 03 },
+        { "Employee Name": "Bob", "E-Mail": "bob@email.com", "ID #": 04 },
+        { "Employee Name": "Suzie", "E-Mail": "suzie@email.com", "ID #": 05 },
+        { "Employee Name": "Stan", "E-Mail": "stan@email.com", "ID #": 06 },
+    ];
+
+    $('#users').html(tableTeamplate({ array: users }));
+});
